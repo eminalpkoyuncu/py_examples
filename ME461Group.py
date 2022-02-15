2ecac8f073999db77d7efe2c70832db1cf2efa70
eminalpkoyuncu <61837425+eminalpkoyuncu@users.noreply.github.com>
2022-02-15 04:00:11 +0300

    Update ME461Group.py

--- ME461Group.py
+++ ME461Group.py
@@ -20,9 +20,25 @@ class ME461Group:
         self.name = userName # your object will be given a user name, i.e. your group name
         self.maxStep = maxStepSize # maximum length of the returned path from run()
         self.maxTime = maxTime # run() is supposed to return before maxTime
-        self.clrDictionary = clrDictionary
 
     def run(self, img, info):
+        clrDictionary = {
+        'black':((1,1,1), 0, 13),
+        'clr100':((225, 1, 1), 100, 1),
+        'clr50':((1, 255, 1), 50, 2), 
+        'clr30':((1, 1, 255), 30, 2),
+        'clr20':((200, 200, 1), 20, 2),
+        'clr10':((255, 1, 255), 10, 2), 
+        'clr9':((1, 255, 255), 9, 3),
+        'clr8':((1,1,150), 8, 3),
+        'clr7':((120,120,40), 7, 3),
+        'clr6':((150,1,150), 6, 3),
+        'clr5':((1,150,150), 5, 3),
+        'clr4':((222,55,222), 4, 3),
+        'clr3':((1, 99, 55), 3, 3),
+        'clr2':((200, 100, 10),2, 3),
+        'clr1':((100, 10, 200),1, 3)
+        }
         myinfo = info[self.name]
         imS = img.shape[0] # assume square image and get size
         loc, game_point = info[self.name]
@@ -33,34 +49,34 @@ class ME461Group:
         newMap = np.zeros((15,15),dtype=int)
         for i in range(7):
             for j in range(7):
-                if img[75+100*i,75+100*j,0] == self.clrDictionary['clr100'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr100'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr100'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr100'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr50'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr50'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr50'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr50'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr30'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr30'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr30'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr30'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr20'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr20'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr20'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr20'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr10'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr10'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr10'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr10'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr9'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr9'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr9'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr9'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr8'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr8'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr8'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr8'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr7'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr7'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr7'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr7'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr6'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr6'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr6'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr6'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr5'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr5'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr5'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr5'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr4'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr4'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr4'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr4'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr3'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr3'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr3'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr3'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr2'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr2'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr2'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr2'][1]
-                elif img[75+100*i,75+100*j,0] == self.clrDictionary['clr1'][0][0] and img[75+100*i,75+100*j,1] == self.clrDictionary['clr1'][0][1] and img[75+100*i,75+100*j,2] == self.clrDictionary['clr1'][0][2]:
-                    newMap[2*i+1,2*j+1] = self.clrDictionary['clr1'][1]
+                if img[75+100*i,75+100*j,0] == clrDictionary['clr100'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr100'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr100'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr100'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr50'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr50'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr50'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr50'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr30'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr30'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr30'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr30'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr20'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr20'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr20'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr20'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr10'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr10'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr10'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr10'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr9'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr9'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr9'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr9'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr8'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr8'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr8'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr8'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr7'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr7'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr7'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr7'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr6'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr6'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr6'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr6'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr5'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr5'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr5'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr5'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr4'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr4'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr4'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr4'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr3'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr3'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr3'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr3'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr2'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr2'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr2'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr2'][1]
+                elif img[75+100*i,75+100*j,0] == clrDictionary['clr1'][0][0] and img[75+100*i,75+100*j,1] == clrDictionary['clr1'][0][1] and img[75+100*i,75+100*j,2] == clrDictionary['clr1'][0][2]:
+                    newMap[2*i+1,2*j+1] = clrDictionary['clr1'][1]
         num_neighbor = 2
 
         left = max(0,yonepix-num_neighbor)
