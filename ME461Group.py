c1c39d21c5b4bcb02fc72e18590d201499921d2e
eminalpkoyuncu <61837425+eminalpkoyuncu@users.noreply.github.com>
2022-02-18 02:31:40 +0300

    Update ME461Group.py

--- ME461Group.py
+++ ME461Group.py
@@ -76,6 +76,10 @@ class ME461Group:
           for j in range(7):
             neighborMap[2*i+3,2*j+3] =  newMap[2*i+3,2*j+3]*(0.75*(newMap[2*i+3-2,2*j+3-2] + newMap[2*i+3+2,2*j+3-2] + newMap[2*i+3-2,2*j+3+2] + newMap[2*i+3+2,2*j+3+2]) + newMap[2*i+3,2*j+3-2] + newMap[2*i+3,2*j+3+2] + newMap[2*i+3-2,2*j+3] + newMap[2*i+3+2,2*j+3])
             distanceMap[2*i+3,2*j+3] = (abs(75+100*i-y) +abs(75+100*j-x))/50
+            
+            if distanceMap[2*i+3,2*j+3] == 0:
+                distanceMap[2*i+3,2*j+3]=9999
+              
             Target_map[2*i+3,2*j+3] = (newMap[2*i+3,2*j+3] + 0.05*neighborMap[2*i+3,2*j+3]) / distanceMap[2*i+3,2*j+3]**2
                 
             for gInd, gName in enumerate(other_groups):
